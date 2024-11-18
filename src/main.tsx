9b029115f74c94ed053ac4626b79bc5a7e9bb930
neebneeba <neebneeba>
2024-11-19 00:32:58 +0900

    chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 // CSS
 import "./index.css";
@@ -17,8 +18,10 @@ import router from "./router";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <ToastContainer />
-      <RouterProvider router={router} />
+      <ChakraProvider value={defaultSystem}>
+        <ToastContainer />
+        <RouterProvider router={router} />
+      </ChakraProvider>
     </Provider>
   </StrictMode>
 );
