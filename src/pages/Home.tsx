73a6992996422d8d44f54e58d3068cba4cd2fca0
neebneeba <neebneeba>
2024-11-24 23:43:38 +0900

    tailwind css  removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,13 @@ import { FC } from "react";
 // Layout
 import Layout from "@/layout";
 
+// Components
+import { Box } from "@chakra-ui/react";
+
 const Home: FC = () => {
   return (
-    <Layout>
-      <div className="bg-red-500 flex">
-        <div className="m-auto">this is Home</div>
-      </div>
+    <Layout title="Нүүр хуудас">
+      <Box>this is Home</Box>
     </Layout>
   );
 };
